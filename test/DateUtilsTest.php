cd1a8e118d3f861a3030db2f468e15f4f7ea1788
Derek <deezerdeezer@gmail.com>
2014-01-31 23:15:33 -0500

    setup phpunit to work with Travis CI

--- test/DateUtilsTest.php
+++ test/DateUtilsTest.php
@@ -17,7 +17,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 */
-include_once('../classes/DateUtils.php');
+include_once('classes/DateUtils.php');
 class DateUtilsTest extends PHPUnit_Framework_TestCase
 {
     private $dateUtils;
